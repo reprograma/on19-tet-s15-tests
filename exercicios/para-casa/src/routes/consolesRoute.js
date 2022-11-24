5246e0a3b817799f7e46cb8d972f163d821156e8
pâmela santos <pssantos201@gmail.com>
2022-11-24 07:49:16 -0300

    :rocket: add: routes

---
+++ exercicios/para-casa/src/routes/consolesRoute.js
@@ -0,0 +1,15 @@
+const controller = require('../controllers/consolesController')
+
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/all', controller.findAllConsoles)
+router.get("/console/:available", controller.findConsoleByAvailable)
+router.get("/search/:developer", controller.findDeveloper)
+router.get("/:id", controller.findConsoleById)
+router.post("/add", controller.addNewConsole)
+router.patch("/:id", controller.updateConsole);
+router.delete("/:id", controller.deleteConsole);
+
+module.exports = router
---
+++ exercicios/para-casa/src/routes/gamesRoute.js
@@ -0,0 +1,15 @@
+const controller = require('../controllers/gamesController')
+
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/all', controller.findAllGames);
+router.get("/search/:gamebygenre", controller.findByGenre);
+router.get("/:name", controller.findGameByName);
+router.get("/:id", controller.findGameById);
+router.post("/add", controller.addNewGame);
+router.patch("/:id", controller.updateGame);
+router.delete("/:id", controller.deleteGame);
+
+module.exports = router
